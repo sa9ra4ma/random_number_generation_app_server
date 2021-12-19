691b37df28d3b57c4ca06c6b9ea1b1916972d341
sa9ra4ma <hishihishi16006@gmail.com>
2021-12-19 16:28:55 +0900

    ファイルの読み書き追加

--- api/random_number.js
+++ api/random_number.js
@@ -2,11 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
+const fs = require('fs');
+const RANDOM_NUMBERS_FILE = './randomNumber.csv';
+
 // 乱数取得
-// GET  http://localhost:3000/api/randomnumber
+// GET  http://[HOST]:[PORT]/api/randomnumber
 router.get('/', async (req, res) => {
     try {
-        const randomNumberList = [13,67,33,55,90];
+        const fileValue = fs.readFileSync(RANDOM_NUMBERS_FILE, 'utf-8');
+        console.log(fileValue)
         res.json(randomNumberList);
     } catch (e) {
         console.error(e);
@@ -15,9 +19,20 @@ router.get('/', async (req, res) => {
 })
 
 // 乱数発行
-// POST  http://localhost:3000/api/randomnumber
+// POST  http://[HOST]:[PORT]/api/randomnumber
 router.post('/', async (req, res) => {
     try {
+        const randomNumberList = [];
+        for (let i = 0; i < 5; i++) {
+            const randomNumber = Math.floor( Math.random() * 100 ) + 1;
+            if (randomNumberList.includes(randomNumber)) {
+                i--;
+                continue;
+            }
+            randomNumberList.push(randomNumber);
+        }
+        const randomNumberStr = randomNumberList.join(',');
+        fs.writeFileSync(RANDOM_NUMBERS_FILE, randomNumberStr, 'utf-8');
         res.status(200).send();
     } catch (e) {
         console.error(e);
