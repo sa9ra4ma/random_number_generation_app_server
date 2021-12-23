e3644fa04b5cf4be37157c751cf38ef0f74901ef
sa9ra4ma <hishihishi16006@gmail.com>
2021-12-23 20:41:21 +0900

    毎回違う値が取得されちゃうので、修正

--- api/theme.js
+++ api/theme.js
@@ -3,19 +3,36 @@ const express = require('express');
 const router = express.Router();
 
 const fs = require('fs');
-const THEME_FILE = './theme.txt';
+const THEME_LIST_FILE = './theme_list.txt';
+const RANDOM_THEME_FILE = './randomTheme.txt';
 
-// テーマランダム取得
-// GET  http://[HOST]:[PORT]/api/theme/random
-router.get('/random', async (req, res) => {
+// テーマ取得
+// GET  http://[HOST]:[PORT]/api/theme
+router.get('/', async (req, res) => {
     try {
-        const fileValue = fs.readFileSync(THEME_FILE, 'utf-8');
-        const themeList = fileValue.split('\r\n');
-        const randomNumber = Math.floor( Math.random() * themeList.length );
+        const theme = fs.readFileSync(RANDOM_THEME_FILE, 'utf-8');
         const themeObj = {
-            theme: themeList[randomNumber]
+            theme
         };
         res.json(themeObj);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send();
+    }
+})
+
+// テーマランダム設定
+// POST  http://[HOST]:[PORT]/api/theme/random
+router.post('/random', async (req, res) => {
+    try {
+        const fileValue = fs.readFileSync(THEME_LIST_FILE, 'utf-8');
+        const themeList = fileValue.split('\r\n');
+        const randomNumber = Math.floor( Math.random() * themeList.length );
+        const theme = themeList[randomNumber];
+        const themeObj = {
+            theme
+        };
+        res.status(200).json(themeObj);
     } catch (e) {
         console.error(e);
         res.status(500).send();
